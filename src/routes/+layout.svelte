a9c8ffaea055039e096817c76c90fa94694f172d
hinryd <lkhh@protonmail.com>
2023-10-20 00:41:49 +0800

    feat: disable ssr

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
 	import 'uno.css';
 	import '@unocss/reset/tailwind-compat.css';
-	import { page } from '$app/stores';
-	import { slide } from 'svelte/transition';
 </script>
 
-{#key $page.url.pathname}
-	<main transition:slide>
-		<slot />
-	</main>
-{/key}
+<main>
+	<slot />
+</main>
